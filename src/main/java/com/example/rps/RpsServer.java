44ffe433125c69f4698271913965bcd95b15514b
Torgash <Lesha2134@gmail.com>
2024-03-26 21:52:24 +0400

    VirtualThreadPerTaskExecutor solution

--- src/main/java/com/example/rps/RpsServer.java
+++ src/main/java/com/example/rps/RpsServer.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,11 @@ public class RpsServer {
     @Value("${spring.application.server.port}")
     private int PORT;
     private final Matchmaker matchmaker;
+    // "Brian Goetz in his famous book "Java Concurrency in Practice" recommends the following formula:
+    // Number of threads = Number of Available Cores * (1 + Wait time / Service time)
+    // https://engineering.zalando.com/posts/2019/04/how-to-set-an-ideal-thread-pool-size.html
+    // Virtual Threads in Java 21 - https://docs.oracle.com/en/java/javase/21/core/virtual-threads.html#GUID-BEC799E0-00E9-4386-B220-8839EA6B4F5C
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
 
     @PostConstruct
     public void startServer() {
@@ -29,9 +36,7 @@ public class RpsServer {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println(STR."New client connected: \{clientSocket.getInetAddress()}");
                 Player newPlayer = new Player(clientSocket);
-                // Start a new thread to handle the client
-                Thread clientThread = new Thread(new RpsPlayerHandler(matchmaker, newPlayer));
-                clientThread.start();
+                executorService.execute(new RpsPlayerHandler(matchmaker, newPlayer));
             }
         } catch (IOException e) {
             System.err.println(STR."Error starting server: \{e.getMessage()}");
