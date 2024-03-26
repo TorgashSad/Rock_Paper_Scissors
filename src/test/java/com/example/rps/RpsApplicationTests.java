051f356e4debc35fa665fdc2294d7e0ac71da391
Torgash <Lesha2134@gmail.com>
2024-03-27 00:11:29 +0400

    Basic integration test

--- src/test/java/com/example/rps/RpsApplicationTests.java
+++ src/test/java/com/example/rps/RpsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.rps;
 
+import org.apache.commons.net.telnet.TelnetClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class RpsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void simpleConnect() throws IOException {
+		TelnetClient telnetClient = new TelnetClient();
+		telnetClient.connect("192.168.1.8", 8888);
+		assertTrue(telnetClient.isConnected());
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(telnetClient.getInputStream()));
+		assertEquals("Welcome to Rock-Paper-Scissors game! Please enter your name.", bufferedReader.readLine());
+		telnetClient.disconnect();
 	}
 
 }
